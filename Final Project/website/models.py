3845532f18e39da2917cb6bfb82df207daab37c8
itsankk05 <aank.ag1234@gmail.com>
2023-03-12 14:20:50 +0530

    deletion of comments added

--- Final Project/website/models.py
+++ Final Project/website/models.py
@@ -10,6 +10,7 @@ class User(db.Model, UserMixin):
     date_created = db.Column(db.DateTime(timezone=True), default= func.now())
     posts = db.relationship('Post', backref='user', passive_deletes=True)
     comments = db.relationship('Comment', backref='user', passive_deletes=True)  
+    likes = db.relationship('likes', backref='user', passive_deletes=True)  
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key= True)
@@ -17,7 +18,7 @@ class Post(db.Model):
     date_created = db.Column(db.DateTime(timezone=True), default= func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
     comments = db.relationship('Comment', backref='post', passive_deletes=True)  
-
+    likes = db.relationship('likes', backref='post', passive_deletes=True)  
 
 class Comment(db.Model):
         id = db.Column(db.Integer, primary_key= True)
@@ -26,5 +27,10 @@ class Comment(db.Model):
         author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
         post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
 
+class Like(db.Model):
+        id = db.Column(db.Integer, primary_key= True)
+        author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+        post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
+        date_created = db.Column(db.DateTime(timezone=True), default= func.now())
 
 
--- Final Project/website/views.py
+++ Final Project/website/views.py
@@ -78,6 +78,23 @@ def create_comment(post_id):
     
     return redirect(url_for('views.home'))
 
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash("Comment does not exist", category="error")
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash("You do not permission to delete", category="error")
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+        
+    return redirect(url_for('views.home'))
+
+
+
 
 
 
