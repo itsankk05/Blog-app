b810dbd5ac34261599ee6bd56ff8f09ed17e6f73
itsankk05 <aank.ag1234@gmail.com>
2023-03-13 17:30:52 +0530

    Likes added

--- Final Project/website/__init__.py
+++ Final Project/website/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     db.init_app(app)
     
     from .views import views
-    from .models import User, Post, Comment
+    from .models import User, Post, Comment, Like
     from .auth import auth
 
     app.register_blueprint(views, url_prefix= "/")
--- Final Project/website/models.py
+++ Final Project/website/models.py
@@ -10,7 +10,7 @@ class User(db.Model, UserMixin):
     date_created = db.Column(db.DateTime(timezone=True), default= func.now())
     posts = db.relationship('Post', backref='user', passive_deletes=True)
     comments = db.relationship('Comment', backref='user', passive_deletes=True)  
-    likes = db.relationship('likes', backref='user', passive_deletes=True)  
+    likes = db.relationship('Like', backref='user', passive_deletes=True)  
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key= True)
@@ -18,7 +18,7 @@ class Post(db.Model):
     date_created = db.Column(db.DateTime(timezone=True), default= func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
     comments = db.relationship('Comment', backref='post', passive_deletes=True)  
-    likes = db.relationship('likes', backref='post', passive_deletes=True)  
+    likes = db.relationship('Like', backref='post', passive_deletes=True)  
 
 class Comment(db.Model):
         id = db.Column(db.Integer, primary_key= True)
